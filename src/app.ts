ab2aa6e4e4e6414774849d423b45a7f2692fd56f
Denison Kolling <denisonkolling@gmail.com>
2024-02-01 10:53:08 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application } from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+class App {
+	private app: Application;
+  private http: http.Server;
+  private io: Server;
+
+	constructor() {
+		this.app = express();
+    this.http = new http.Server(this.app)
+    this.io = new Server(this.http)
+
+	}
+}
+
+export { App };
