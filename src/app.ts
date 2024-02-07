4a32a8becf42216ab0e6fb470f3580c2ddaee2b5
Denison Kolling <denisonkolling@gmail.com>
2024-02-07 13:09:21 -0300

    chore: add cors origins

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,15 @@ class App {
 	constructor() {
 		this.app = express();
 		this.http = new http.Server(this.app);
-		this.io = new Server(this.http);
+		this.io = new Server(this.http, {
+			cors: {
+				origin: '*',
+			},
+		});
 	}
 
 	public listen() {
-		this.app.listen(3333, () => {
+		this.http.listen(3333, () => {
 			console.log('Server running on port 3333');
 		});
 	}
@@ -26,6 +30,7 @@ class App {
 	private socketEvents(socket: Socket) {
 		console.log('Socket connected: ' + socket.id);
 		socket.on('subscribe', (data) => {
+			console.log('User added to room: ' + data.roomId)
 			socket.join(data.roomId);
 
 			socket.broadcast.to(data.roomId).emit('chat', {
